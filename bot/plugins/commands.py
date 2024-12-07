1e8fdbbbbc5bfafee4250337243e356e93739beb
Labani Behera <101978891+ZxLbni@users.noreply.github.com>
2024-12-07 15:00:11 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -1,15 +1,18 @@
-#pylint:disable=E0602
+# pylint:disable=E0602
 from telethon import Button
 from telethon.events import NewMessage
 from telethon.tl.custom.message import Message
+
 from bot import TelegramBot
 from bot.config import Telegram
 from bot.modules.static import *
 from bot.modules.decorators import verify_user
 
+
 @TelegramBot.on(NewMessage(incoming=True, pattern=r'^/start$'))
 @verify_user(private=True)
 async def welcome(event: NewMessage.Event | Message):
+    sender = await event.get_sender()
     await event.reply(
         message=WelcomeText % {"first_name": sender.first_name},
         buttons=[
@@ -20,10 +23,13 @@ async def welcome(event: NewMessage.Event | Message):
         ],
     )
 
+
 @TelegramBot.on(NewMessage(incoming=True, pattern=r'^/info$'))
 @verify_user(private=True)
 async def user_info(event: Message):
-    await event.reply(UserInfoText.format(sender=event.sender))
+    sender = await event.get_sender()
+    await event.reply(UserInfoText.format(sender=sender))
+
 
 @TelegramBot.on(NewMessage(chats=Telegram.OWNER_ID, incoming=True, pattern=r'^/log$'))
 async def send_log(event: NewMessage.Event | Message):
